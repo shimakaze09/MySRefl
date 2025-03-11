e8430624d8cd495f5ff9f61be1306f00df616685
John <yangtianzhuo970425@gmail.com>
2025-03-11 14:46:57 +1300

    Refactor Type structures to replace subclasses with bases and update field handling

--- src/test/05_base/main.cpp
+++ src/test/05_base/main.cpp
@@ -31,9 +31,10 @@ template <>
 struct Type<A> {
   static constexpr std::string_view name = "A";
   using type = A;
-  static constexpr TypeList subclasses = {};
+  static constexpr TypeList bases = {};
 
-  static constexpr FieldList fields = FieldList{Field{"a", &A::a, AttrList{}}};
+  static constexpr FieldList fields =
+      FieldList{Field{"a", &A::a, AttrList{}}}.UnionTypeList(bases);
 
   static constexpr AttrList attrs = {};
 };
@@ -42,9 +43,10 @@ template <>
 struct Type<B> {
   static constexpr std::string_view name = "B";
   using type = B;
-  static constexpr TypeList subclasses = {};
+  static constexpr TypeList bases = {};
 
-  static constexpr FieldList fields = FieldList{Field{"b", &B::b, AttrList{}}};
+  static constexpr FieldList fields =
+      FieldList{Field{"b", &B::b, AttrList{}}}.UnionTypeList(bases);
 
   static constexpr AttrList attrs = {};
 };
@@ -53,9 +55,10 @@ template <>
 struct Type<C> {
   static constexpr std::string_view name = "C";
   using type = C;
-  static constexpr TypeList subclasses = {Type<A>{}};
+  static constexpr TypeList bases = {Type<A>{}};
 
-  static constexpr FieldList fields = FieldList{Field{"c", &C::c, AttrList{}}};
+  static constexpr FieldList fields =
+      FieldList{Field{"c", &C::c, AttrList{}}}.UnionTypeList(bases);
 
   static constexpr AttrList attrs = {};
 };
@@ -64,9 +67,10 @@ template <>
 struct Type<D> {
   static constexpr std::string_view name = "D";
   using type = D;
-  static constexpr TypeList subclasses = {Type<B>{}, Type<C>{}};
+  static constexpr TypeList bases = {Type<B>{}, Type<C>{}};
 
-  static constexpr FieldList fields = FieldList{Field{"d", &D::d, AttrList{}}};
+  static constexpr FieldList fields =
+      FieldList{Field{"d", &D::d, AttrList{}}}.UnionTypeList(bases);
 
   static constexpr AttrList attrs = {};
 };
@@ -76,13 +80,12 @@ void dump(size_t depth = 0) {
   for (size_t i = 0; i < depth; i++)
     cout << "  ";
   cout << Type<T>::name << endl;
-  Type<T>::subclasses.ForEach(
+  Type<T>::bases.ForEach(
       [depth](auto t) { dump<typename decltype(t)::type>(depth + 1); });
 }
 
 int main() {
   dump<D>();
 
-  // TODO: walk recursively
   Type<D>::fields.ForEach([](auto field) { cout << field.name << endl; });
 }
