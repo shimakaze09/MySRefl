2c1dd9dc112c0ebbed8867142b3a9177076349bf
John <yangtianzhuo970425@gmail.com>
2025-03-13 15:28:14 +1300

    Bump version to 0.5.2; add static constexpr names for constructor, destructor, and default value in Util.h, and update AutoRefl parsing logic to use these names

--- include/MySRefl/Util.h
+++ include/MySRefl/Util.h
@@ -9,6 +9,16 @@ template <typename Signature>
 constexpr auto WrapConstructor();
 template <typename T>
 constexpr auto WrapDestructor();
+
+namespace Name {
+static constexpr const char constructor[] = "__constructor";
+static constexpr const char destructor[] = "__destructor";
+static constexpr const char default_value[] =
+    "__default_value";  // function argument default value
+static constexpr const char name[] = "__name";  // function argument name
+// compile time
+#define MY_MYSREFL_NAME_ARG(x) "__arg_" #x
+}  // namespace Name
 }  // namespace My::MySRefl
 
 #include "detail/Util.inl"
