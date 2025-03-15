fed4466de671b713e56a556370bcfe7ed0d04a03
John <yangtianzhuo970425@gmail.com>
2025-03-15 23:05:50 +1300

    Remove commented-out condition for params in AutoRefl::Parse to clean up code

--- src/AutoRefl/AutoRefl.cpp
+++ src/AutoRefl/AutoRefl.cpp
@@ -177,7 +177,7 @@ string AutoRefl::Parse(string_view code) {
           ss << "&" << type << "::" << funcInfo.name;
       }
 
-      if (!funcInfo.metas.empty() || !funcInfo.params.empty()) {
+      if (!funcInfo.metas.empty() /*|| !funcInfo.params.empty()*/) {
         ss << "," << endl << indent << indent << indent << "AttrList {" << endl;
         for (const auto& [key, value] : funcInfo.metas) {
           ss << indent << indent << indent << indent << "Attr{" << "\"" << key
